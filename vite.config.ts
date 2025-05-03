339fd333fa33990314724e6ab1e3b0c712dc1bc3
Polo <148433691+MugishaProsper@users.noreply.github.com>
2025-05-03 17:54:16 +0200

    routing error

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        }
+      }
+    }
   },
   server: {
     proxy: {
