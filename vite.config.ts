a97a89ad982c277ded664634fc046e1db54cdf59
Polo <148433691+MugishaProsper@users.noreply.github.com>
2025-05-03 17:41:28 +0200

    redirects error resolved

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
