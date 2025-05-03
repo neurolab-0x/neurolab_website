3ea08223dee1f08d3d1e7db955a0902529e19d5b
Polo <148433691+MugishaProsper@users.noreply.github.com>
2025-05-03 16:58:49 +0200

    backend configs for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['axios']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/]
-    }
-  },
   server: {
-    port: 5173,
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp'
-    },
     proxy: {
       '/api': {
         target: 'https://web-backend-ivhv.onrender.com',
         changeOrigin: true,
-        secure: false
+        secure: false,
       }
     }
   }
