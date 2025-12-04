21570380ca0fbf5ca0e09e42efe033caa261a422
aine1100 <ainedushimire@gmail.com>
2025-12-04 11:58:16 +0200

    [fixes]

--- __tests__/components/header.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { Header } from '@/components/header'
-describe('Header Component', () => {
-  it('renders the header component', () => {
-    render(<Header />)
-    // Add your assertions here
-    expect(screen.getByRole('banner')).toBeInTheDocument()
-  })
-})
